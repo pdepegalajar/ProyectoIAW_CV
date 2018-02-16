da3986f9683e524f539347e9bab4e061f5d78808
pdepegalajar <36546109+pdepegalajar@users.noreply.github.com>
2018-02-16 23:53:40 +0100

    Update html2pdf.php

--- html2pdf.php
+++ html2pdf.php
@@ -9,7 +9,7 @@
  * @author    Laurent MINGUET <webmaster@html2pdf.fr>
  * @copyright 2017 Laurent MINGUET
  */
-require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__).'/html2pdf/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;

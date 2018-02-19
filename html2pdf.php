baee82181e1d47799a290fa3ac912565dda1887e
pdepegalajar <36546109+pdepegalajar@users.noreply.github.com>
2018-02-19 22:08:10 +0100

    Update html2pdf.php

--- html2pdf.php
+++ html2pdf.php
@@ -20,7 +20,7 @@ try {
     include dirname(__FILE__).'/res/creadorCV.php';
     $content = ob_get_clean();
 
-    $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', 3);
+    $html2pdf = new Html2Pdf('P', 'A4', 'es', true, 'UTF-8', 3);
     $html2pdf->pdf->SetDisplayMode('fullpage');
     $html2pdf->writeHTML($content);
     $html2pdf->output('CurriculumVitae.pdf');
